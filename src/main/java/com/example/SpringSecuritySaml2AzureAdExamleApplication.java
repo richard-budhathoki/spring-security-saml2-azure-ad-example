34048e6bd8128e2667832727953db1cbe7e56e60
Lance Li <lance.jx.li@gtomato.com>
2023-11-28 17:42:34 +0800

    Initial commit

---
+++ src/main/java/com/example/SpringSecuritySaml2AzureAdExamleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysaml2azureadexamle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySaml2AzureAdExamleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySaml2AzureAdExamleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/SpringSecuritySaml2AzureAdExamleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritysaml2azureadexamle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySaml2AzureAdExamleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
